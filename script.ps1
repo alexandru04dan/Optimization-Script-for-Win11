05ea89fd2d9f4ea230ee8bf855d22c35afa72d1d
Alexandru Dan <alexandrudan04@outlook.com>
2025-05-08 11:55:09 +0300

    Added option to check if script is succesfull

--- script.ps1
+++ script.ps1
@@ -1,5 +1,7 @@
 #Setting power configuration for better performance
 powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+powercfg /l
 
+Write-Output "If there is a star displayed on the High Performance line, then this script was executed succesfully"
    
 Pause
