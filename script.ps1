7c52cd57c18837423326977ec8d823fc286201c7
Alexandru Dan <alexandrudan04@outlook.com>
2025-05-09 14:43:32 +0300

    Added option to turn off animations

--- script.ps1
+++ script.ps1
@@ -47,4 +47,20 @@ else {
     Write-Host "Invalid input. Please enter Y or N."
 }
 
+#Disable animations and visual effects
+$responseAnimations = Read-Host "Do you wish to disable animations and visual effects? (y/n)"
+
+$responseAnimations = $responseAnimations.Trim().ToLower()
+
+if ($responseAnimations -eq 'y' -or $responseAnimations -eq 'yes') {
+    Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\WindowMetrics" -Name "MinAnimate" -Value 0
+    Write-Host "Animations and visual effects are now turned off!"
+}
+elseif ($responseAnimations -eq 'n' -or $responseAnimations -eq 'no'){
+    Write-Host "Animations and visuall effects are not turned off :("
+}
+else {
+    Write-Host "Invalid input. Please enter Y or N."
+}
+
 Read-Host -Prompt "Press Enter to exit"
